e7e1021b5a02542290e991722c3ef7ca5185518c
oriol1901 <boomjack1901@gmail.com>
2017-02-02 04:01:52 +0100

    AJAX Status and error handling implemented.

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
 import {loadCourses} from './actions/courseActions';
 import {loadAuthors} from './actions/authorActions';
+
 
 const store = configureStore();
 store.dispatch(loadCourses());
